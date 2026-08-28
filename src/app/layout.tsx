ddf6fcaa806fd29691b1846064d18430c99102ce
Nicolas Borboën <ponsfrilus@gmail.com>
2026-08-28 17:56:51 +0200

    [feature] Add umami tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -26,6 +27,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       <body className="h-dvh w-dvw overflow-hidden antialiased">
         {children}
       </body>
+      <Script
+        src="https://umami.fsd.epfl.ch/script.js"
+        data-website-id="2acf40b7-8b27-4426-bae5-16ece05b6dab"
+        strategy="afterInteractive"
+      />
     </html>
   );
 }
